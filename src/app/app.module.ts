59aad1f6491c3757cea45765fcf0b9eab5f5864a
Marcus Schuster <marcus.schuster@vkb.de>
2020-09-18 10:21:19 +0200

    Using rxangular push pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
-import { ReactiveComponentModule } from '@ngrx/component';
+import { PushModule } from '@rx-angular/template';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
-import { createCustomElement } from '@angular/elements';
 import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [AppComponent, ButtonComponent, PostComponent],
-  imports: [BrowserModule, ReactiveComponentModule],
+  imports: [BrowserModule, PushModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {
-
   constructor(injector: Injector) {
     // customElements.define(
     //   'local-post',
